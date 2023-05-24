72bac7297c4fadfee09a838cc2f9bce6b5708c81
slekup <74949101+slekup@users.noreply.github.com>
2023-05-25 02:13:19 +1200

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# slekup-npm
+# Slekup
+
+Suite of generic tools used by Slekup.
+
+### Profile card
+
+See my profile links by running the following command:
+
+```bash
+npx slekup
+```
---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import colors from 'colors';
+
+console.log(`
+${colors.magenta(
+  `*********** | ${colors.bold(colors.white('Slekup'))} | ***********`
+)}
+
+${colors.green('Website')}: ${colors.blue('https://slekup.com')}
+${colors.green('GitHub')}: ${colors.blue('https://github.com/slekup')}
+`);
